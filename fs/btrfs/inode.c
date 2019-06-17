41b53469e07722e5843dc48caa04390edfa40695
freak07 <michalechner92@googlemail.com>
2019-06-17 15:40:04 +0200

    Revert "do d_instantiate/unlock_new_inode combinations safely"
    This reverts commit f440ea85d429e59f63d626e017403cb09d9adbdb.

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6657,7 +6657,8 @@ static int btrfs_mknod(struct inode *dir, struct dentry *dentry,
 		goto out_unlock_inode;
 	} else {
 		btrfs_update_inode(trans, root, inode);
-		d_instantiate_new(dentry, inode);
+		unlock_new_inode(inode);
+		d_instantiate(dentry, inode);
 	}
 
 out_unlock:
@@ -6734,7 +6735,8 @@ static int btrfs_create(struct inode *dir, struct dentry *dentry,
 		goto out_unlock_inode;
 
 	BTRFS_I(inode)->io_tree.ops = &btrfs_extent_io_ops;
-	d_instantiate_new(dentry, inode);
+	unlock_new_inode(inode);
+	d_instantiate(dentry, inode);
 
 out_unlock:
 	btrfs_end_transaction(trans);
@@ -6881,7 +6883,12 @@ static int btrfs_mkdir(struct inode *dir, struct dentry *dentry, umode_t mode)
 	if (err)
 		goto out_fail_inode;
 
-	d_instantiate_new(dentry, inode);
+	d_instantiate(dentry, inode);
+	/*
+	 * mkdir is special.  We're unlocking after we call d_instantiate
+	 * to avoid a race with nfsd calling d_instantiate.
+	 */
+	unlock_new_inode(inode);
 	drop_on_err = 0;
 
 out_fail:
@@ -10561,7 +10568,8 @@ static int btrfs_symlink(struct inode *dir, struct dentry *dentry,
 		goto out_unlock_inode;
 	}
 
-	d_instantiate_new(dentry, inode);
+	unlock_new_inode(inode);
+	d_instantiate(dentry, inode);
 
 out_unlock:
 	btrfs_end_transaction(trans);
